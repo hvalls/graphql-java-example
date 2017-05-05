9215e09c6aee7ab76ea74f8d48503d01243476f4
hvalls <hectorvallsmondragon@gmail.com>
2017-05-05 21:09:43 +0200

    Add types contract

--- src/main/java/com/example/GQLController.java
+++ src/main/java/com/example/GQLController.java
@@ -4,6 +4,7 @@ import graphql.ExecutionResult;
 import graphql.GraphQL;
 import graphql.GraphQLError;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +21,7 @@ public class GQLController {
     @Autowired
     GraphQL graphQL;
 
-    @PostMapping("/graphql")
+    @PostMapping(value = "/graphql", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody Map<String, Object> graphql(@RequestBody String body) {
         ExecutionResult result = graphQL.execute(body);
         List<GraphQLError> errors = result.getErrors();
